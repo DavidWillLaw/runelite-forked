3b1f73fb9bd13d8fdee4bcfa083d047bf8791035
DavidWillLaw <51643431+DavidWillLaw@users.noreply.github.com>
2024-03-04 22:50:28 +0000

    Added extra information sending

--- runelite-client/src/main/java/net/runelite/client/plugins/al_karid_fisher/al_karid_fisher.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/al_karid_fisher/al_karid_fisher.java
@@ -1,13 +1,15 @@
-package net.runelite.client.plugins.pfplugin;
+package net.runelite.client.plugins.al_karid_fisher;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.inject.Provides;
 import net.runelite.api.*;
 import net.runelite.api.coords.LocalPoint;
+import net.runelite.api.coords.WorldPoint;
+import net.runelite.api.widgets.ComponentID;
+import net.runelite.api.widgets.Widget;
+import net.runelite.api.widgets.WidgetID;
 import net.runelite.client.config.ConfigManager;
-import net.runelite.client.eventbus.Subscribe;
-import net.runelite.client.events.ConfigChanged;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 import net.runelite.api.Client;
@@ -15,9 +17,9 @@ import net.runelite.api.GameObject;
 import net.runelite.api.Perspective;
 import net.runelite.api.Point;
 import net.runelite.api.Tile;
+import net.runelite.api.widgets.InterfaceID;
 
 import java.awt.*;
-import java.io.OutputStream;
 import java.net.*;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -30,33 +32,22 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
+import java.util.List;
 
 import com.google.gson.Gson; // Ensure you have Gson in your project dependencies
 
 @PluginDescriptor(
-        name = "PF Plugin",
+        name = "Al Karid Fishing",
         description = "Does some stuff.",
         enabledByDefault = false
 )
 
-public class pfplugin extends Plugin
+public class al_karid_fisher extends Plugin
 {
-    @Inject
-    private pfconfig config;
-
-    @Provides
-    pfconfig getConfig(ConfigManager configManager)
-    {
-        return configManager.getConfig(pfconfig.class);
-    }
-
     private final HttpClient httpClient = HttpClient.newHttpClient();
     private final Gson gson = new Gson();
     private static final String SERVER_URL = "http://localhost:5000/receive_data";
-    private static final Set<Integer> TREE_IDS = Set.of(1276, 1277, 1278, 1279, 1280);
-    private static final Set<Integer> OAK_TREE_IDS = Set.of(10820);
-    private static final Set<Integer> WILLOW_TREE_IDS = Set.of(10833, 10829, 10819, 10831);
-    private static final Set<Integer> TIN_COPPER_ORE = Set.of(10943, 11161, 11360, 11361);
+    private static final Set<Integer> SMALL_NET_FISHING_SPOT = Set.of(1528);
     private ScheduledExecutorService executorService;
 
     @Inject
@@ -66,8 +57,6 @@ public class pfplugin extends Plugin
     protected void startUp() throws Exception {
         executorService = Executors.newSingleThreadScheduledExecutor();
         executorService.scheduleAtFixedRate(this::compileAndSendData, 0, 200, TimeUnit.MILLISECONDS); // Poll twice per second
-
-        sendConfigUpdate();
     }
 
     @Override
@@ -75,6 +64,19 @@ public class pfplugin extends Plugin
     {
         System.out.println("Plugin shutting down!");
         executorService.shutdown();
+    }
+
+    final int BANK_GROUP_ID = 12; // Placeholder value, replace with the actual ID
+    final int BANK_CONTAINER_ID = 1; // Placeholder value, replace with the actual ID
+
+    // Returns whether the bank is open or not, returns True/False
+    private boolean isBankOpen() {
+        Widget w = client.getWidget(ComponentID.BANK_CONTAINER);
+        if (w != null){
+            return true;
+        } else {
+            return false;
+        }
     }
 
     // Gets the inventory count and array
@@ -122,23 +124,57 @@ public class pfplugin extends Plugin
     }
 
     // Gets the bounding boxes of trees
-    private JsonArray gatherTreeLocations() {
-        JsonArray treeBoundingBoxes = new JsonArray();
+    private JsonArray gatherFishLocations() {
+        JsonArray fishingSpotLocations = new JsonArray();
+        List<NPC> npcs = client.getNpcs();
+        Set<Integer> npcIds = SMALL_NET_FISHING_SPOT; // Make sure this is defined correctly
+
+        int boxWidth = 50; // Width of the bounding box, adjust as needed
+        int boxHeight = 50; // Height of the bounding box, adjust as needed
+
+        for (NPC npc : npcs) {
+            if (npc != null && npcIds.contains(npc.getId())) {
+                WorldPoint worldLocation = npc.getWorldLocation();
+                if (worldLocation != null) {
+                    LocalPoint localPoint = LocalPoint.fromWorld(client, worldLocation);
+                    if (localPoint != null) {
+                        Point screenPoint = Perspective.localToCanvas(client, localPoint, client.getPlane());
+
+                        if (screenPoint != null) {
+                            // Calculate the top-left and bottom-right points of the bounding box
+                            int topLeftX = screenPoint.getX() - boxWidth / 2;
+                            int topLeftY = screenPoint.getY() - boxHeight / 2;
+                            int bottomRightX = screenPoint.getX() + boxWidth / 2;
+                            int bottomRightY = screenPoint.getY() + boxHeight / 2;
+
+                            JsonObject json = new JsonObject();
+                            json.addProperty("topLeftX", topLeftX);
+                            json.addProperty("topLeftY", topLeftY);
+                            json.addProperty("bottomRightX", bottomRightX);
+                            json.addProperty("bottomRightY", bottomRightY);
+                            fishingSpotLocations.add(json);
+                        }
+                    }
+                }
+            }
+        }
+        return fishingSpotLocations;
+    }
+
+
+    private JsonArray gatherBankBoothLocations() {
+        JsonArray bankBoothBoundingBoxes = new JsonArray();
         Tile[][][] tiles = client.getScene().getTiles();
 
         // Fetch user-specified GameObject IDs from the plugin configuration
-        String gameObjectIdsConfig = config.gameObjectIds(); // Assuming 'config' is your @Inject'ed ExamplePluginConfig instance
-        Set<Integer> gameObjectIds = Arrays.stream(gameObjectIdsConfig.split(","))
-                .map(String::trim)
-                .map(Integer::parseInt)
-                .collect(Collectors.toSet());
+        final Set<Integer> BankBooth = Set.of(10355);
 
         for (Tile[][] plane : tiles) {
             for (Tile[] xCoord : plane) {
                 for (Tile tile : xCoord) {
                     if (tile != null) {
                         for (GameObject gameObject : tile.getGameObjects()) {
-                            if (gameObject != null && gameObjectIds.contains(gameObject.getId())) {
+                            if (gameObject != null && BankBooth.contains(gameObject.getId())) {
                                 Shape clickbox = gameObject.getClickbox();
                                 if (clickbox != null) {
                                     Rectangle bounds = clickbox.getBounds();
@@ -147,7 +183,7 @@ public class pfplugin extends Plugin
                                     json.addProperty("topLeftY", bounds.getMinY());
                                     json.addProperty("bottomRightX", bounds.getMaxX());
                                     json.addProperty("bottomRightY", bounds.getMaxY());
-                                    treeBoundingBoxes.add(json);
+                                    bankBoothBoundingBoxes.add(json);
                                 }
                             }
                         }
@@ -155,9 +191,8 @@ public class pfplugin extends Plugin
                 }
             }
         }
-        return treeBoundingBoxes;
+        return bankBoothBoundingBoxes;
     }
-
 
     // Gets the state of the player, animating and ismoving
     private JsonObject gatherPlayerStates() {
@@ -165,10 +200,19 @@ public class pfplugin extends Plugin
         Player localPlayer = client.getLocalPlayer();
         boolean isAnimating = localPlayer != null && localPlayer.getAnimation() != -1;
         boolean isInteracting = localPlayer != null && localPlayer.isInteracting();
+        System.out.println("Checking if bank is open...");
+        boolean isBankOpen = isBankOpen(); // Adjust based on how you implement the check
+        System.out.println("Bank open: " + isBankOpen);
         int walkAnimation = localPlayer.getWalkAnimation();
+        // Get the player world location
+        Player player = client.getLocalPlayer();
+        WorldPoint playerWorldLocation = player.getWorldLocation();
 
         playerStates.addProperty("isAnimating", isAnimating);
         playerStates.addProperty("isInteracting", isInteracting);
+        playerStates.addProperty("x", playerWorldLocation.getX());
+        playerStates.addProperty("y", playerWorldLocation.getY());
+        playerStates.addProperty("isBankOpen", isBankOpen);
 
         //System.out.println("isAnimating : " + isAnimating);
         //System.out.println("isInteracting : " + isInteracting);
@@ -176,6 +220,50 @@ public class pfplugin extends Plugin
 
         return playerStates;
     }
+
+    // Gets the tiles around the player
+    private JsonArray gatherSurroundingTiles() {
+        JsonArray tileArray = new JsonArray();
+        final int MAX_DISTANCE = 25; // Adjust the distance as needed
+        Player player = client.getLocalPlayer();
+
+        if (player == null) {
+            return tileArray;
+        }
+
+        WorldPoint playerWorldLocation = player.getWorldLocation();
+        if (playerWorldLocation == null) {
+            return tileArray;
+        }
+
+        int baseX = playerWorldLocation.getX() - MAX_DISTANCE;
+        int baseY = playerWorldLocation.getY() - MAX_DISTANCE;
+
+        for (int dx = 0; dx <= MAX_DISTANCE * 2; dx++) {
+            for (int dy = 0; dy <= MAX_DISTANCE * 2; dy++) {
+                int worldX = baseX + dx;
+                int worldY = baseY + dy;
+                LocalPoint localPoint = LocalPoint.fromWorld(client, worldX, worldY);
+
+                if (localPoint != null) {
+                    Point screenPoint = Perspective.localToCanvas(client, localPoint, client.getPlane());
+                    if (screenPoint != null) {
+                        JsonObject tileObject = new JsonObject();
+                        tileObject.addProperty("worldX", worldX);
+                        tileObject.addProperty("worldY", worldY);
+                        tileObject.addProperty("screenX", screenPoint.getX());
+                        tileObject.addProperty("screenY", screenPoint.getY());
+                        // Add your logic to determine if the tile is walkable
+                        tileObject.addProperty("walkable", true); // Placeholder
+                        tileArray.add(tileObject);
+                    }
+                }
+            }
+        }
+
+        return tileArray;
+    }
+
 
     // Combines various bits of data into a single JSON, then sends this to the sendToPythonServer method that sends the JSON
     public void compileAndSendData() {
@@ -196,10 +284,16 @@ public class pfplugin extends Plugin
 
             //System.out.println("Compiled data before trees : " + compiledData);
 
-            // Gather tree locations
-            JsonArray treeLocations = gatherTreeLocations();
-            compiledData.add("trees", treeLocations);
+            // Gather fishing locations
+            JsonArray fishLocations = gatherFishLocations();
+            compiledData.add("fishing_spots", fishLocations);
 
+            // Gather fishing locations
+            JsonArray bankBoothLocations = gatherBankBoothLocations();
+            compiledData.add("bank_booths", bankBoothLocations);
+
+            JsonArray tileData = gatherSurroundingTiles();
+            compiledData.add("tiles", tileData);
             //System.out.println("Full compiled data : " + compiledData);
 
             // Send the compiled JSON
@@ -222,36 +316,9 @@ public class pfplugin extends Plugin
             System.out.println("Response from server: " + response.body());
         } catch (IOException | InterruptedException e) {
             System.out.println("Failure in sendToPythonServer || Is Server On?");
-        }
-    }
-
-    // Code that triggers on a config change updating itemsToDrop then sending that to the Python script
-    @Subscribe
-    public void onConfigChanged(ConfigChanged event) throws IOException {
-        if (!"pfplugin".equals(event.getGroup()))
-        {
-            return;
-        }
-
-        sendConfigUpdate();
-    }
-
-    public void sendConfigUpdate() {
-        String itemIdsToDrop = config.itemIdsToDrop();
-        String jsonInputString = "{\"itemIdsToDrop\": \"" + itemIdsToDrop + "\"}";
-        System.out.println("Config update sending : " + jsonInputString);
-
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("http://localhost:5000/update_settings"))
-                .header("Content-Type", "application/json")
-                .POST(HttpRequest.BodyPublishers.ofString(jsonInputString))
-                .build();
-
-        try {
-            HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-            System.out.println("Response from server: " + response.body());
-        } catch (IOException | InterruptedException e) {
-            System.out.println("An error occurred while sending config update: " + e.getMessage());
+            e.printStackTrace();
+            // Restore interrupted state...
+            Thread.currentThread().interrupt();
         }
     }
 
